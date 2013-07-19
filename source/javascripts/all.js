1f96104b6a483907df05d630c882ae0e1fb19802
jvillarejo <arzivian87@gmail.com>
2013-07-19 16:36:37 -0300

    My web page intialized

---
+++ source/javascripts/all.js
@@ -0,0 +1,48 @@
+//= require_tree .
+
+$(document).ready(function() {
+	var speed = 10;
+
+	$('section').each(function() {
+		var $backgroundObject = $(this);
+
+		$(window).scroll(function() {
+			var scrollTop = $(window).scrollTop();
+			var yPosition = -(scrollTop / speed);
+			var coords = '50% ' + yPosition + 'px';
+			$backgroundObject.css({ backgroundPosition: coords});
+
+			handleBackToTop(scrollTop);
+		})
+	});
+
+
+	$('nav a').each(function() {
+		var button = $(this);
+
+		button.click(function() { 
+			var sectionName = button.attr('href');
+			scrollTo(sectionName);
+		});
+	});
+});
+
+var fading = false;
+
+function handleBackToTop(scrollTop) {
+	if(scrollTop > 100) {
+		$('body > header').addClass('pinned');
+	} else if(scrollTop <= 100) {
+		$('body > header').removeClass('pinned');
+	}
+}
+
+function onFadeComplete() { 
+	fading = false;
+}
+
+function scrollTo(sectionName) {
+	var topOffset = $(sectionName).offset().top;
+	$('html, body').animate({scrollTop:topOffset},'5000');
+}
+
